9a5b78a14788ee9575c9b0d20d067105c96b76a8
pedro <pedromonteirobittencourt@gmail.com>
2024-07-17 16:45:54 -0300

    lesson #3

---
+++ README.md
@@ -0,0 +1,8 @@
+# Local Library Exercise
+
+Developed from the MDN web docs as a learning exercise for Express.js
+https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs
+
+## Description
+
+The Local Library Exercise is a web application that allows users to manage a local library. It provides features such as browsing books, searching for books, and keeping track of borrowed books.
---
+++ app.js
@@ -0,0 +1,63 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const expressLayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayout);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// connect to mongoose
+mongoose.set('strictQuery', false);
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const mongoDB = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.anxedxt.mongodb.net/local-library?retryWrites=true&w=majority&appName=Cluster0`;
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(mongoDB);
+    console.log('Sucessfully connected to MongoDB.');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+connectToDatabase();
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
